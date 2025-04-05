da7663a5832c20b21c82b20d1e5f380434588318
shinobu <155656201+Ada-Church-Closure@users.noreply.github.com>
2025-04-05 13:19:04 +0800

    Update logger.c

--- logger.c
+++ logger.c
@@ -3,21 +3,17 @@
 #include <time.h>
 #include <errno.h>
 
-//����һ���ļ���
 static FILE* logFile = NULL;
 static LogLevel currentLevel = LOG_INFO;
 
 bool logger_init(const char* logFilePath) {
 	if (logFilePath) {
-		//׷��ģʽ��������û�д���
 		errno err = fopen(&logFile, logFilePath, "a");
 		if (err != 0 || !logFile) {
-			//���ļ�ʧ��
 			return false;
 		}
 	}
 	else {
-		//��׼���
 		logFile = stdout;
 	}
 	return true;
@@ -31,18 +27,14 @@ void logger_close(void) {
 }
 
 void log_message(LogLevel level, const char* format, ...) {
-	//��־����С�����õļ�������ļ�û�д�
 	if (currentLevel < level || !logFile) {
 		return;
 	}
 	
-	//��ȡʱ���
 	time_t now = time(NULL);
 	struct tm now_tm;
 	localtime(&now_tm, &now);
 
-	//��ӡʱ�������־����
-	//���Ǽ򵥵İ����ʱ����Ϣ���뵽�ļ�����ȥ
 	fprintf(logFile, "[%d-%02d-%02d %02d:%02d:%02d]",
 		now_tm.tm_year + 1900,
 		now_tm.tm_mon + 1,
@@ -61,15 +53,12 @@ void log_message(LogLevel level, const char* format, ...) {
 	default: fprintf(logFile, "[UNKNOWN]");	break;
 	}
 
-	//�����ɱ����
 	va_list args;
 	va_start(args, format);
-	//����Ϣ�����logFile��
 	vfprintf(logFile, format, args);
 	va_end(args);
 
 	fprintf(logFile, "\n");
-	//ȷ����־��Ϣ������д���ļ�
 	fflush(logFile);
 }	
 
